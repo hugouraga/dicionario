5bb67bf5e02825dacb33fa321502c05712f0dbab
hugo issao uraga <hugouraga61@gmail.com>
2018-05-17 11:45:23 -0300

    primeiro commit

---
+++ dicionario.py
@@ -0,0 +1,19 @@
+class No:
+    def __init__(self,chave=None,item=None,proximo=None):
+        self.chave = chave
+        self.valor = valor
+        self.proximo = proximo
+
+class dicionario:
+    def __init__(self):
+        self.primeiro = self.ultimo = No()
+    def vazia():
+        return self.primeiro == self.ultimo
+    def inserir(chave,valor):
+        aux = self.primeiro.proximo
+        while not aux in None and aux.chave != chave:
+            aux = aux.proximo
+        if aux is None:
+            aux = No(chave, valor, None)
+        else:
+            aux.valor = valor
